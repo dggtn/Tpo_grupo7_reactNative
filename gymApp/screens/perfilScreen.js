14999f509c0829c4474b322ac914d1b315272282
Daniela <dggtn97@gmail.com>
2025-10-11 13:22:52 -0300

    creo la pantalla para el perfil la hago igual que catalogo de cursos y despues la voy a cambiar

---
+++ gymApp/screens/perfilScreen.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, FlatList, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import { useTheme } from 'react-native-paper';
+
+export default function perfilScreen() {
+    const navigation = useNavigation()
+    const url = process.env.EXPO_PUBLIC_API_URL
+
+    const [isLoading, setLoading] = useState(true)
+    const [cursos, setCursos] = useState([])
+
+    const getCursos = async () => {
+        try {
+            const response =  await fetch(url + '/courses/allCourses', {
+                method: 'GET'
+            })
+
+            const json = await response.json()
+            setCursos(json.data)
+        } catch(error) {
+            console.log("ERROR, ", error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getCursos()        
+    }, [])
+
+    const Curso = ({cursoName, onPress}) => (
+        <TouchableOpacity style={styles.item} onPress={onPress}>
+            <Text style={styles.title}>{cursoName}</Text>
+        </TouchableOpacity>
+    )
+
+    return (
+        <View>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    data={cursos}
+                    renderItem={({item}) => <Curso cursoName={item.name} onPress={() => navigation.navigate('DetalleCurso', {idCurso: item.id})}/> }
+                    keyExtractor={item => item.id}
+                />
+            )}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
